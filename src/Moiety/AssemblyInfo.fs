ba9ee34876f78664c425b3114c714e3450b587ed
Casey Kramer <casey.kramer@gmail.com>
2014-02-13 22:46:30 -0500

    Update assembly version

--- src/Moiety/AssemblyInfo.fs
+++ src/Moiety/AssemblyInfo.fs
@@ -7,7 +7,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitle("Moiety")>]
 [<assembly: AssemblyDescription("Delimited Value File parser")>]
 [<assembly: AssemblyCopyright("Copyright © Casey Kramer 2012")>]
-[<assembly: AssemblyVersion("0.8.*")>]
-[<assembly: AssemblyFileVersion("0.8.*")>]
+[<assembly: AssemblyVersion("1.0.0.2")>]
+[<assembly: AssemblyFileVersion("1.0.0.2")>]
 
 do()
